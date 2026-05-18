d5f627099cf9ae40d053dc853c6737e18d7452dd
JCPrime01 <vanzokiller@gmail.com>
2026-05-18 19:54:33 -0300

    Update redirect URL in index.js

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=2KCTZTV7YMF&afp2=tapi100f' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=2KCTZTV7YMF&afp2=tapig' });
     res.end();
   }
